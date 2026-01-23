26d0b4d4b12debba97cdb7fdce1db6508755a7b5
Nuno Fernandes <nuno.o.fernandes@outlook.pt>
2026-01-23 00:09:13 +0000

    feat: add signin github backstage

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -27,8 +27,6 @@ backend.add(import('@backstage/plugin-techdocs-backend'));
 backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
-backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-// See https://backstage.io/docs/auth/guest/provider
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
